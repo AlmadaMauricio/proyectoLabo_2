cd6f67d643eb08863fc30d76b5459d936f9eddec
mauriciogaston.almada <mauriciogaston.almada@gmail.com>
2022-10-22 11:39:39 -0300

    subiendo clase fecha
    primera subida con la clase fecha.

---
+++ proyecto_labo2/Fecha.h
@@ -0,0 +1,24 @@
+#pragma once
+class Fecha
+{
+private:
+	int _dia;
+	int _mes;
+	int _anio;
+public:
+	Fecha(int, int, int);
+
+	int getDia();
+	int getMes();
+	int getAnio();
+
+	void setDia(int dia);
+	void setMes(int mes);
+	void setAnio(int anio);
+
+	void Cargar(int, int, int);
+	void Mostrar();
+
+
+};
+
