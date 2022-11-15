403852bc5be77951098f04d94cbab951328f381b
Majo <illanes.mariajose@gmail.com>
2022-11-15 10:17:27 -0300

    Correcciones en Pais OK

--- proyecto_labo2/Pais.cpp
+++ proyecto_labo2/Pais.cpp
@@ -19,11 +19,6 @@ void Pais::setEstado(bool estado) {
 }
 
 
-int Pais::getIdPais()
-{
-	return 0;
-}
-
 ///Getters
 const char* Pais::getNombre() {
 	return _Nombre;
@@ -32,25 +27,92 @@ const char* Pais::getNombre() {
 bool Pais::getEstado() {
 	return _Estado;
 }
-
 void Pais::CargarPais()
 {
-}
+	int aux;
+	char nom[50];
 
-void Pais::MostrarPais()
-{
+	do
+	{
+		cout << "Ingrese ID de Pais";
+		cin >> aux;
+		if (aux < 1)
+		{
+			cout << "El ID no puede ser negativo." << endl;
+			anykey();
+		}
+	} while (aux < 1);
+
+	this->setId(aux);
+
+	cout << "Ingrese nombre de pais: ";
+	cin.ignore();
+	cin.getline(_Nombre, 49);
+
+	this->setEstado(true);
+
+}
+int Pais::getIdPais() {
+	return _IdPais;
 }
 
 bool Pais::grabarEnDisco()
 {
-	return false;
+	FILE* fReg = fopen("paises.dat", "ab");
+
+	if (fReg == NULL)
+	{
+		cout << "No se puede abrir el archivo.";
+		system("PAUSE < null");
+		return false;
+	}
+
+	int escribio = fwrite(this, sizeof(Pais), 1, fReg);
+
+	fclose(fReg);
+
+	return escribio;
 }
 
-bool Pais::leerDeDisco(int)
+bool Pais::leerDeDisco(int pos)
 {
-	return false;
+	FILE* fReg = fopen("paises.dat", "rb");
+
+	if (fReg == NULL)
+	{
+		cout << "No se puede abrir el archivo.";
+		system("PAUSE < null");
+		return false;
+	}
+
+	fseek(fReg, pos * sizeof(Pais), SEEK_SET);
+
+	int escribio = fread(this, sizeof(Pais), 1, fReg);
+
+	fclose(fReg);
+
+	return escribio;
 }
 
-void mostrarPais(int pais)
-{
-}
+void mostrarPais(int pais) {
+	switch (pais)
+	{
+	case 1:
+		cout << "ARGENTINA";
+		break;
+	case 2:
+		cout << "BRASIL";
+		break;
+	case 3:
+		cout << "USA";
+		break;
+	case 4:
+		cout << "RUSIA";
+		break;
+	case 5:
+		cout << "ALEMANIA";
+		break;
+	default:
+		break;
+	}
+};
