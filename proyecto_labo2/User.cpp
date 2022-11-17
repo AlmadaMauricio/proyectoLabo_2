a58e0d6b1f00bde569d17f84e35ec4c2eadbe183
majoillanes <85851174+majoillanes@users.noreply.github.com>
2022-11-17 01:23:35 -0300

    correccion fecha user

--- proyecto_labo2/User.cpp
+++ proyecto_labo2/User.cpp
@@ -897,7 +897,7 @@ void ordenarVectorPorFecha(User* vec, int tam) {
 
 	for (int i = 0; i < tam - 1; i++) {
 		for (int j = i + 1; j < tam; j++) {
-			if (vec[j].getFechaIngreso() > vec[i].getFechaIngreso()) {
+			if (vec[i].getFechaIngreso() > vec[j].getFechaIngreso()) {
 				aux = vec[i];
 				vec[i] = vec[j];
 				vec[j] = aux;
